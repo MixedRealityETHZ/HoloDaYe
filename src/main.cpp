6f0731e82a3768273f2f60824825e22bcd0777f5
Bin Yang <ybMaschinenbau@gmail.com>
2022-12-12 21:01:13 +0100

    add message format for sending back to client

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <sstream>
 #include "holodaye/utils.h"
+#include <iomanip>
 #include <list>
 
 
@@ -18,33 +19,33 @@ using namespace std;
 
 void main()
 {		
-	// Test Algo
-	// float x = 19840;
-	// float y = -14240;
-    float x = 2683691;
-    float y = 1247833;
-	// float z = 465.52;
-	float angle = 0;
-	int length = 100;
-    ElevationData *data = new ElevationData();
-    cout << "line 27" << endl;
-	// ElevationData* data = new ElevationData();
-    cout << data->getResolution() << endl;
-    cout << "line 28" << endl;
-	ElevationAngle eleAngle(length, x, y, data);
-	eleAngle.FindPeakInCircle();
-	cout << "line 31" << endl;
+	// // Test Algo
+	// // float x = 19840;
+	// // float y = -14240;
+    // float x = 2683691;
+    // float y = 1247833;
+	// // float z = 465.52;
+	// float angle = 0;
+	// int length = 100;
+    // ElevationData *data = new ElevationData();
+    // cout << "line 27" << endl;
+	// // ElevationData* data = new ElevationData();
+    // cout << data->getResolution() << endl;
+    // cout << "line 28" << endl;
+	// ElevationAngle eleAngle(length, x, y, data);
+	// eleAngle.FindPeakInCircle();
+	// cout << "line 31" << endl;
     
-	list <list<double>> result;	
-	for (int i = 0; i < 30; i++){
-		x = eleAngle.border_d_[i] * cos(i / 180 * M_PI);
-		y = eleAngle.border_d_[i] * cos(i / 180 * M_PI);
-		Struct1 gps = LV952GPS(x, y, eleAngle.border_h_[i]);
-		result.push_back({gps.lat, gps.lon, gps.alt});
-	}
+	// list <list<double>> result;	
+	// for (int i = 0; i < 30; i++){
+	// 	x = eleAngle.border_d_[i] * cos(i / 180 * M_PI);
+	// 	y = eleAngle.border_d_[i] * cos(i / 180 * M_PI);
+	// 	Struct1 gps = LV952GPS(x, y, eleAngle.border_h_[i]);
+	// 	result.push_back({gps.lat, gps.lon, gps.alt});
+	// }
 
-	cout << "line 41" << endl;
-	printNestedList(result);
+	// cout << "line 41" << endl;
+	// printNestedList(result);
 
 	// test
 	// double lat = 47.37602004225053;
@@ -61,143 +62,164 @@ void main()
 	// cout << "longitude is:" << gps.lon << " and latitude is " << gps.lat;
 
 	// ----------------------------------------------server----------------------------------------
-// 	// Initialze winsock
-// 	WSADATA wsData;
-// 	WORD ver = MAKEWORD(2, 2);
+	// Initialze winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
 
-// 	int wsOk = WSAStartup(ver, &wsData);
-// 	if (wsOk != 0)
-// 	{
-// 		cerr << "Can't Initialize winsock! Quitting" << endl;
-// 		return;
-// 	}
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Can't Initialize winsock! Quitting" << endl;
+		return;
+	}
 	
-// 	// Create a socket
-// 	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
 	
-// 	if (listening == INVALID_SOCKET)
-// 	{
-// 		cerr << "Can't create a socket! Quitting" << endl;
-// 		return;
-// 	}
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Can't create a socket! Quitting" << endl;
+		return;
+	}
 
-// 	// Bind the ip address and port to a socket
-// 	sockaddr_in hint;
-// 	hint.sin_family = AF_INET;
-// 	hint.sin_port = htons(54000);
-// 	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
 	
-// 	bind(listening, (sockaddr*)&hint, sizeof(hint));
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
 
-// 	// Tell Winsock the socket is for listening 
-// 	listen(listening, SOMAXCONN);
-// 	//cout << listen(listening, SOMAXCONN) << endl;
+	// Tell Winsock the socket is for listening 
+	listen(listening, SOMAXCONN);
+	//cout << listen(listening, SOMAXCONN) << endl;
 
-// 	// Wait for a connection
-// 	sockaddr_in client;
-// 	int clientSize = sizeof(client);
+	// Wait for a connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
 
 
 
-// 	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-// 	//cout << clientSocket << endl;
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	//cout << clientSocket << endl;
 
-// 	char host[NI_MAXHOST];		// Client's remote name
-// 	char service[NI_MAXSERV];	// Service (i.e. port) the client is connect on
+	char host[NI_MAXHOST];		// Client's remote name
+	char service[NI_MAXSERV];	// Service (i.e. port) the client is connect on
 
-// 	ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-// 	ZeroMemory(service, NI_MAXSERV);
+	ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
 
-// 	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-// 	{
-// 		cout << host << " connected on port " << service << endl;
-// 	}
-// 	else
-// 	{
-// 		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-// 		cout << host << " connected on port " <<
-// 			ntohs(client.sin_port) << endl;
-// 	}
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connected on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " <<
+			ntohs(client.sin_port) << endl;
+	}
 
-// 	// Close listening socket
-// 	closesocket(listening);
+	// Close listening socket
+	closesocket(listening);
 	
 	
 	
 
-// 	// While loop: accept and echo message back to client
-// 	char buf[4096];
+	// While loop: accept and echo message back to client
+	char buf[4096];
 
-// 	while (true)
-// 	{	
+	while (true)
+	{	
 		
 
-// 		ZeroMemory(buf, 4096);
+		ZeroMemory(buf, 4096);
 
-// 		// Wait for client to send data
-// 		int bytesReceived = recv(clientSocket, buf, 4096, 0);
-// 		if (bytesReceived == SOCKET_ERROR)
-// 		{
-// 			cerr << "Error in recv(). Quitting" << endl;
-// 			//continue;
-// 			break;
-// 		}
+		// Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cerr << "Error in recv(). Quitting" << endl;
+			//continue;
+			break;
+		}
 
-// 		if (bytesReceived == 0)
-// 		{
-// 			cout << "Client disconnected " << endl;
-// 			break;
-// 			//continue;
-// 		}
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected " << endl;
+			break;
+			//continue;
+		}
 
-// 		string received = string(buf, 0, bytesReceived);
-// 		cout << received << endl;
+		string received = string(buf, 0, bytesReceived);
+		cout << received << endl;
 
-// 		// Read the data from cliend and process the data
-// 		vector<float> vect;
+		// Read the data from cliend and process the data
+		vector<float> vect;
 
-// 		stringstream ss(received);
+		stringstream ss(received);
 
-// 		for (float i; ss >> i;) {
-// 			vect.push_back(i);
-// 			if (ss.peek() == ',')
-// 				ss.ignore();
-// 		}
-// 		for (size_t i = 0; i < vect.size(); i++)
-// 			cout << vect[i] << endl;
+		for (double i; ss >> i;) {
+			vect.push_back(i);
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+		for (size_t i = 0; i < vect.size(); i++)
+			cout << setprecision(15) << vect[i] << endl;
 
-// // ----------------------------------------------server----------------------------------------
-// 		// Echo message back to client
-// 		//send(clientSocket, buf, bytesReceived + 1, 0);
+// ----------------------------------------------server----------------------------------------
+		// Echo message back to client
+		// send(clientSocket, buf, bytesReceived + 1, 0);
 
-// 		// Send message to client
-// 		//string userInput;
-// 		//getline(cin, userInput);
-// 		//int sendResult = send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+		// Send message to client
+		// string userInput;
+		// getline(cin, userInput);
+		// int sendResult = send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
 
-// 		// Call holodaye function
+		// Call holodaye function
 		
-// 		// string message_1 = to_string(eleAngle.border_d_[10]) + " Woshinidie ";
+		// string message_1 = to_string(eleAngle.border_d_[10]) + " Woshinidie ";
 
-// 		// string message_2 = to_string(eleAngle.border_h_[10]) + " Woyoubushinidie ";
+		// string message_2 = to_string(eleAngle.border_h_[10]) + " Woyoubushinidie ";
 
 
-// 		// string message_3 = to_string(eleAngle.max_slope_[10]) + " CaiCaiwoshishei ";
+		// string message_3 = to_string(eleAngle.max_slope_[10]) + " CaiCaiwoshishei ";
 
-// 		// string message = message_1 + message_2 + message_3;
-// 		// //cout << message << endl;
-// 		// send(clientSocket, message.c_str(), message.size() + 1, 0);
+		// string message = message_1 + message_2 + message_3;
+		// //cout << message << endl;
+		// send(clientSocket, message.c_str(), message.size() + 1, 0);
 
-// 		// Close the socket
+
+		// Convert the result to string stream and send back to client
+		list<string> message_list;
+		list <list<double>> result({{1.0896564839292, 47.37602004225053, 8.546809259609395}, {24.0896789839292, 33.39872004225053, 12.546890259609395}});
+		for (auto& outer : result)
+		{
+			for (auto& inner : outer)
+			{
+    			stringstream ss;
+				ss << fixed << setprecision(15) << inner;
+				message_list.push_back(ss.str());
+			}
+		}
+
+		ostringstream stream;
+
+		copy(message_list.begin(), message_list.end(), ostream_iterator<string>(stream, ","));
+		string message = stream.str();
+
+		send(clientSocket, message.c_str(), message.size() + 1, 0);
+
+		// // Close the socket
 		
 	
-// 	}
-// 	closesocket(clientSocket);
+	}
+	closesocket(clientSocket);
 	
 	
 
-// 	// Cleanup winsock
-// 	WSACleanup();
+	// Cleanup winsock
+	WSACleanup();
 
-// 	system("pause");
+	system("pause");
 }
