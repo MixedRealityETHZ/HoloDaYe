bef128fd1d4682d140edd553838ef4de3bc2ac97
zengpy17 <zengyuqi0307@126.com>
2022-12-16 11:59:57 +0100

    [merge] Merge utils.h from other branch

---
+++ include/holodaye/utils.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+
+
+
+
+struct GPS {
+    double lon, lat, alt;
+};
+
+struct LV95 {
+    int E, N;
+};
+
+typedef struct GPS Struct1;
+typedef struct LV95 Struct2;
+
+// Convert LV95 format to GPS format
+Struct1 LV952GPS(int E, int N, double H);
+// Convert GPS format to LV95 format
+Struct2 GPS2LV95(double lon, double lat);
+
+// Print nested list
+void printNestedList(list<list<double> > nested_list);
