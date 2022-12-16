65acd24fc3a6945481e2dbf68ba3adea10e18f07
zengpy17 <zengyuqi0307@126.com>
2022-12-16 16:41:53 +0100

    [update] Finish test

--- test/uetliberg.cpp
+++ test/uetliberg.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 int main(){
-    LV95 coord = GPS2LV95(8.491263136328094,47.350187074002775);
-    GPS gps = LV952GPS(coord.E,coord.N,0);
+    LV95 coord = GPS2LV95(8.491038526435288,47.35020711144787);//wechat
+    GPS gps = LV952GPS(coord.E,coord.N);
     coord = GPS2LV95(gps.lon,gps.lat);
     std::cout<<coord.E<<" "<<coord.N<<std::endl;
     ElevationData data;
-    std::cout<<data.get_elevation(coord.E, coord.N)<<std::endl;
+    std::cout<<data.get_elevation(coord.E, coord.N)<<" "<< LV952GPS(coord.E, coord.N,data.get_elevation(coord.E, coord.N)).alt<<std::endl;
 }
