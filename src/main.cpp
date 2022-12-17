15b8084bb41edec660596719df24c90af1edad73
Bin Yang <ybMaschinenbau@gmail.com>
2022-12-17 13:42:32 +0100

    input curret z from gps

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 // Main function, although empty now
 #include <iostream>
 #include <holodaye/elevation.h>
+#define _USE_MATH_DEFINES
+#include "holodaye/output.h"
+#include <math.h>
+#include "holodaye/utils.h"
 
 int main(int argc, char **argv){
+	
+	int x = 2683691;
+	int y = 1247833;
+	double z = 553.272;
+	cout << z << endl;
+	int length = 100;
+	cout<< "Reading Data" << endl;
+	ElevationData* data = new ElevationData();
+	cout<< "Finish reading" << endl;		
+	ElevationAngle eleAngle(length, x, y, z, data);
+	eleAngle.FindPeakInCircle();
     std::cout<<"Hello World!"<<std::endl;
     return 0;
 }
